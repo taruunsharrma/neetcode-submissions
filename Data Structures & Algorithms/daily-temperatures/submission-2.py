ed630994ad30ce9f960e126de3d5542dfd0f44ed
Taruun Sharrma <36966951+taruunsharrma@users.noreply.github.com>
2026-06-26 17:32:05 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.py
@@ -0,0 +1,42 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        
+        # [30, 38, 30, 36, 35, 40, 28]
+        # res = [0]*len(temperatures)
+
+        # for i in range(len(temperatures)):
+        #     for j in range(i+1, len(temperatures)):
+        #         if temperatures[i] < temperatures[j]:
+        #             res[i] = j-i
+        #             break
+        #         continue
+        
+        # return res
+
+        # res = [0] * len(temperatures)
+
+        # stack = []      # stores indices
+
+        # for i in range(len(temperatures)):
+
+        #     # Current temperature is warmer
+        #     while stack and temperatures[i] > temperatures[stack[-1]]:
+
+        #         prev = stack.pop()
+
+        #         res[prev] = i - prev
+
+        #     stack.append(i)
+
+        # return res
+
+
+        stack = []
+        res = [0] * len(temperatures)
+        for i,t in enumerate(temperatures):
+            while stack and t > temperatures[stack[-1]]:
+                prev = stack.pop()
+                res[prev] = i - prev
+            
+            stack.append(i)
+        return res
